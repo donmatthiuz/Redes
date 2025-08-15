b84c3d0be458c790228ea0869764fd7c235bd9db
cor22982 <cor22982@uvg.edu.gt>
2025-08-15 08:58:58 -0600

    Complete of all damged

---
+++ algoritmos/DJstra.py
@@ -0,0 +1,35 @@
+
+from connection.Red import Red
+
+class SolveDjstra():
+  def __init__(self, miRed: Red):
+      self.red = miRed
+
+  def minDistance(self, dist, sptSet):
+        min = 1e7
+        for v in range(self.red.V):
+            if dist[v] < min and sptSet[v] == False:
+                min = dist[v]
+                min_index = v
+
+        return min_index
+  def printSolution(self, dist):
+    print("Vertex \t Distance from Source")
+    for node in range(self.red.V):
+       print(node, "\t\t", dist[node])
+
+  def dijkstra(self, src):
+    dist = [1e7] * self.red.V
+    dist[src] = 0
+    sptSet = [False] * self.red.V
+    for cout in range(self.red.V):
+      u = self.minDistance(dist, sptSet)
+      sptSet[u] = True
+      for v in range(self.V):
+          if (self.red.graph[u][v] > 0 and 
+              sptSet[v] == False and 
+              dist[v] > dist[u] + self.red.graph[u][v]):
+              dist[v] = dist[u] + self.red.graph[u][v]
+    self.debugPrint(dist)
+  
+  
