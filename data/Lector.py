24a8f771194b5dca407b2dadd4fd9c509438d1f5
cor22982 <cor22982@uvg.edu.gt>
2025-08-24 01:21:30 -0600

    Lector separado

--- data/Lector.py
+++ data/Lector.py
@@ -1,2 +1,34 @@
-class Lector_Configuracion:
-  
+import json
+
+def create_sample_config():
+    import os
+    os.makedirs("data", exist_ok=True)
+    
+    topo_data = {
+        "type": "topo",
+        "config": {
+            "A": ["B", "C"],
+            "B": ["A", "C", "D"],
+            "C": ["A", "B", "D"],
+            "D": ["B", "C"]
+        }
+    }
+    
+    names_data = {
+        "type": "names",
+        "config": {
+            "A": "nodeA@localhost",
+            "B": "nodeB@localhost",
+            "C": "nodeC@localhost",
+            "D": "nodeD@localhost"
+        }
+    }
+    
+    with open("data/topo.txt", "w") as f:
+        json.dump(topo_data, f, indent=2)
+    
+    with open("data/id_nodos.txt", "w") as f:
+        json.dump(names_data, f, indent=2)
+    
+    print("✅ Archivos de configuración creados en /data/")
+
--- main.py
+++ main.py
@@ -1,40 +1,7 @@
 # va a crear 2 nodos  
 # el .env de la raiz es el usuario/puerto
 from connection.Nodo import Nodo
-
-import json
-def create_sample_config():
-    import os
-    os.makedirs("data", exist_ok=True)
-    
-    topo_data = {
-        "type": "topo",
-        "config": {
-            "A": ["B", "C"],
-            "B": ["A", "C", "D"],
-            "C": ["A", "B", "D"],
-            "D": ["B", "C"]
-        }
-    }
-    
-    names_data = {
-        "type": "names",
-        "config": {
-            "A": "nodeA@localhost",
-            "B": "nodeB@localhost",
-            "C": "nodeC@localhost",
-            "D": "nodeD@localhost"
-        }
-    }
-    
-    with open("data/topo.txt", "w") as f:
-        json.dump(topo_data, f, indent=2)
-    
-    with open("data/id_nodos.txt", "w") as f:
-        json.dump(names_data, f, indent=2)
-    
-    print("✅ Archivos de configuración creados en /data/")
-
+from data.Lector import create_sample_config
 
 if __name__ == "__main__":
     import sys
